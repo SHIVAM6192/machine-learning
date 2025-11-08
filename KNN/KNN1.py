02477bdd75abefbb512074d6807ffe618cccc3e2
Shivam <shivamsagar6192@gmail.com>
2025-11-08 19:58:13 +0530

    Removed space

--- KNN/KNN1.py
+++ KNN/KNN1.py
@@ -15,7 +15,6 @@ def euclidean_distance(p1, p2):
     return math.sqrt((p1[0] - p2[0]) ** 2 + (p1[1] - p2[1]) ** 2)
 
 # KNN algorithm
-
 def knn_predict(test_point, k=3):
     distances = []
     for features, label in data:
